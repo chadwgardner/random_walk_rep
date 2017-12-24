46d244ee39850d6f3cea28a6b4de81788fad5a04
Chad Gardner <gardner.cw@gmail.com>
2017-12-23 22:10:22 -0500

    updated analysis

--- walk_analysis.py
+++ walk_analysis.py
@@ -25,6 +25,40 @@ samples = np.random.normal(mean_dist, std_dist, size=10000)
 x_ther, y_ther = ecdf(samples)
 x_ecdf, y_ecdf = ecdf(distances)
 
+
+#examining avg coordinates and their distribution
+flat_coords = [list(i) for i in x]
+for i in range(len(flat_coords)):
+    flat_coords[i].extend(y[i])
+coords_avg = [np.mean(i) for i in flat_coords]
+coords_avg_avg = np.mean(coords_avg)
+coords_std = np.std(coords_avg)
+coords_normal = np.random.normal(coords_avg_avg, coords_std, size=10000)
+
+#ecdf and pdf of coords
+plt.subplot(2,1,1)
+x_coords_ecdf, y_coords_ecdf = ecdf(coords_avg)
+x_coords_ther, y_coords_ther = ecdf(coords_normal)
+plt.plot(x_coords_ecdf, y_coords_ecdf, marker='.', linestyle='none')
+plt.plot(x_coords_ther, y_coords_ther)
+plt.legend(['mean', 'normal', 'random walk'])
+plt.xlabel('Mean coordinate of walks')
+plt.ylabel('Probability')
+plt.title('CDFs')
+
+#hist of coords
+plt.subplot(2,1,2)
+plt.hist(coords_avg, normed=True, bins=40, alpha=0.40)
+plt.hist(coords_normal, normed=True, bins=40, alpha=0.4, histtype='stepfilled')
+plt.axvline(x=coords_avg_avg, color='r', alpha=0.5)
+plt.legend(['mean', 'normal', 'mean coords'])
+plt.xlabel('Mean coordinates of walks')
+plt.ylabel('Fraction of walks')
+
+"""
+#plotting ecdf and pdf of coords
+
+
 #histogram
 plt.subplot(2,1,1)
 plt.hist(distances, normed=True, bins=40, alpha=0.4)
@@ -41,7 +75,7 @@ plt.plot(x_ther, y_ther)
 plt.xlim(xmin=0)
 plt.axvline(x=mean_dist, color='r', alpha=.4)
 plt.legend(['random walk', 'normal', 'mean'])
-
+"""
 
 
 plt.show()
