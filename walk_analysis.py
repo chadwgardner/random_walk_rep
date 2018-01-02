ed27fe998929b688d6967320582507481b0a7350
Chad Gardner <gardner.cw@gmail.com>
2018-01-01 21:41:33 -0500

    done for x coords

--- walk_analysis.py
+++ walk_analysis.py
@@ -8,22 +8,25 @@ sns.set()
 
 
 #getting walks
-x, y = get_walks(10000, 100)
+x, y = get_walks(100000, 10)
 #x_np = np.array(x)
 #y_np = np.array(y)
 
 #get end points of walks, calculate distances from origin, find mean
 end_points = [[i[-1],j[-1]] for i,j in zip(x, y)]
-distances = np.sqrt([i[0]**2 + i[1]**2 for i in end_points])
-mean_dist = np.mean(distances)
-std_dist = np.std(distances)
+x_distances = [i[0] for i in end_points]
+y_distances = [i[1] for i in end_points]
+x_mean_dist = np.mean(x_distances)
+y_mean_dist = np.mean(y_distances)
+x_std_dist = np.std(x_distances)
+y_std_dist = np.std(y_distances)
 
-conf_int = np.percentile(distances, [2.5,97.5])
+#conf_int = np.percentile(distances, [2.5,97.5])
 
 #prepare normal samples
-samples = np.random.normal(mean_dist, std_dist, size=10000)
+samples = np.random.normal(x_mean_dist, x_std_dist, size=10000)
 x_ther, y_ther = ecdf(samples)
-x_ecdf, y_ecdf = ecdf(distances)
+x_ecdf, y_ecdf = ecdf(x_distances)
 
 
 #examining avg coordinates and their distribution
@@ -61,9 +64,9 @@ plt.ylabel('Fraction of walks')
 
 #histogram
 plt.subplot(2,2,2)
-plt.hist(distances, normed=True, bins=40, alpha=0.4)
-plt.hist(samples, histtype='stepfilled', normed=True, bins=100, alpha=0.4)
-plt.axvline(x=mean_dist, color='r', alpha=0.4)
+plt.hist(x_distances, normed=True, bins=40, alpha=0.4)
+#plt.hist(samples, histtype='stepfilled', normed=True, bins=100, alpha=0.4)
+plt.axvline(x=x_mean_dist, color='r', alpha=0.4)
 plt.legend(['mean', 'normal', 'random walk'])
 plt.xlabel('Distance walked in steps')
 plt.ylabel('Fraction of walks')
@@ -73,7 +76,7 @@ plt.subplot(2,2,4)
 plt.plot(x_ecdf, y_ecdf, marker='.', linestyle='none', alpha=.4)
 plt.plot(x_ther, y_ther)
 plt.xlim(xmin=0)
-plt.axvline(x=mean_dist, color='r', alpha=.4)
+plt.axvline(x=x_mean_dist, color='r', alpha=.4)
 plt.legend(['random walk', 'normal', 'mean'])
 plt.xlabel('Distance walked in steps')
 plt.ylabel('Probability')
